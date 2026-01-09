8ecae20565456dc32463b3b88ffa2030498c3e6f
Soumydip-pal <164073832+Soumydip-pal@users.noreply.github.com>
2026-01-09 17:26:15 +0000

    Variable

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "soumyadippal395@gmail.com"
+var accounPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId =2 // not allowed
+
+// console.log(accountId);
+
+ accountEmail = "hc@hc.com"
+ accounPassword = "212444"
+ accountCity = "Bengaluru"
+
+ /* prefer not to use 'var'
+ because of issue in block scope And functionl scope 
+ */
+
+ console.table([accounPassword,accountEmail,accountEmail,accountCity,accountState])

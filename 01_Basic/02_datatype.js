1a8bdaf9104e8730fbee9db1b532e52083049046
Soumydip-pal <164073832+Soumydip-pal@users.noreply.github.com>
2026-01-09 17:26:37 +0000

    datatype

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser engine so, it will not work
+
+/*Datatypes--- (1) primitive
+                   number-> 2 to power 53
+                   bigint
+                   string->""
+                   boolean->true/false
+                   null-> standalonr value, representation of empty value
+                   undefined->value not assigned
+                   symbol->to identify uniqueness
+                
+                   (2) Non-primitive
+                      objects->
+                       */
+
+                       console.log(typeof undefined); //undefined
+                       console.log(typeof null);//object

23d0087456c3f4fd8f83af195c23e384d854b89f
Soumydip-pal <164073832+Soumydip-pal@users.noreply.github.com>
2026-01-09 18:48:04 +0000

    Operator

---
+++ 01_Basic/04_operator.js
@@ -0,0 +1,22 @@
+let str1 = "hello"
+let str2 = " soumya"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+
+console.log( ( 3 + 4) * 5 % 3);//correct format
+
+console.log(+ true);//1
+//console.log(true+);//error
+console.log(+"");//0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+++gameCounter;//pre-increment
+console.log(gameCounter)//101
